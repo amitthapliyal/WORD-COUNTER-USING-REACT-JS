8cbe409f1960ca14632d46a4e530d30c1fa7df4a
amitthapliyal <103377739+amitthapliyal@users.noreply.github.com>
2022-10-02 23:34:20 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -32,24 +32,23 @@ export default function TextForm(props) {
     const [text, setText] = useState('enter text here');
     return (
         <>
-            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }} >
+            <div className="container" style={{ color: props.mode === 'dark' ? ' grey' : 'white' }} >
                 <h1 >{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn primary mx-2" onClick={handleUpClick} >Convert to upper case</button>
-                <button className="btn btn primary mx-2" onClick={handleloClick}>Convert to lower case</button>
-                <button className="btn btn primary mx-2" onClick={handleclearClick}>Clear</button>
-                <button className="btn btn primary mx-2" onClick={handleCopy}>copy text</button>
+                <button className="btn btn danger mx-2" style = {{background: '#edb4bd'}} onClick={handleUpClick} >Convert to upper case</button>
+                <button className="btn btn primary mx-2" style = {{background: '#edb4bd'}} onClick={handleloClick}>Convert to lower case</button>
+                <button className="btn btn primary mx-2" style = {{background: '#edb4bd'}}onClick={handleclearClick}>Clear</button>
+                <button className="btn btn primary mx-2" style = {{background: '#edb4bd'}}onClick={handleCopy}>copy text</button>
 
             </div>
 
-            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }} >
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? '#9068be ' : '#e1e8f0' }} >
                 <h1>Your text summary</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length}Minutes read</p>
-                <h2>Perivew</h2>
-                <p>{text.length > 0 ? text : "enter something to previewnpm "}</p>
+
             </div>
         </>
     )
