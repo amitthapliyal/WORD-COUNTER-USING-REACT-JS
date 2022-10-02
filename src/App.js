236ccc987e80ea6c247c24f0c7352836de6ef02e
amitthapliyal <103377739+amitthapliyal@users.noreply.github.com>
2022-10-02 23:33:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   const toggleMode=()=>{
     if(mode==='light'){
       setMode  ('dark');
-      document.body.style.backgroundColor='#042743';
+      document.body.style.backgroundColor='black';
     }else{
       setMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor='grey';
     }
   }
   return (
