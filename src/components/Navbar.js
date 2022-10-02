8803020e8b7f5a2100a8bb63f8e7653bdf12be4f
amitthapliyal <103377739+amitthapliyal@users.noreply.github.com>
2022-10-02 23:33:47 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar(props) {
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form> */}
-                    <div className={`form-check form-switch text-${props.mode === 'white' ? '#8458B3' : '#f75990'}`}>
+                    <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
                         <label className="form-check-label" htmlfor="flexSwitchCheckDefault">Dark Mode Enabled</label>
                     </div>
